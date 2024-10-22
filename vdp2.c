2296e4274d8a3d1f6bce30bfb4a42a4b28382f28
Yuma Ohgami <yuma@ohgami.jp>
2024-10-22 22:19:32 +0900

    [WIP] VDP2

--- vdp2.c
+++ vdp2.c
@@ -2,6 +2,11 @@
 
 #define VDP2_TVMD	*(unsigned short *)0x25f80000
 #define VDP2_BGON	*(unsigned short *)0x25f80020
+#define VDP2_PNCN0	*(unsigned short *)0x25f80030
+#define VDP2_PLSZ	*(unsigned short *)0x25f8003a
+#define VDP2_MPOFN	*(unsigned short *)0x25f8003c
+#define VDP2_MPABN0	*(unsigned short *)0x25f80040
+#define VDP2_MPCDN0	*(unsigned short *)0x25f80042
 #define VDP2_PRISA	*(unsigned short *)0x25f800f0
 
 /* VDP2の初期化 */
@@ -20,6 +25,21 @@ void vdp2_init(void) {
 	   - Transparent enable = all enable
 	   - On = all off */
 	VDP2_BGON = 0x0000;
+
+	/* PNCN0 */
+	VDP2_PNCN0 = 0x4123;
+
+	/* PLSZ */
+	VDP2_PLSZ = 0x1234;
+
+	/* MPABN0 */
+	VDP2_MPABN0 = 0x2b1a;
+
+	/* MPCDN0 */
+	VDP2_MPCDN0 = 0x1d3c;
+
+	/* MPOFN */
+	VDP2_MPOFN = 0x1234;
 
 	/* PRISA
 	   - Sprite 0 priority number = 6
