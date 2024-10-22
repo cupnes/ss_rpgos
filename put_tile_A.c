3cb83bb12e8d7133a881b9a50297f09fc8efd2d7
Yuma Ohgami <yuma@ohgami.jp>
2024-10-22 22:19:47 +0900

    [WIP] put tile A

---
+++ put_tile_A.c
@@ -0,0 +1,25 @@
+#define TILE_DATA_ADDR_A	/* TODO */
+
+/* NBG0をタイルモードへ設定 */
+void setup_nbg0_with_tile(void)
+{
+	/* キャラクタサイズを1x1タイルへ設定 */
+	/* TODO */
+
+	/* パターンデータを設定 */
+
+	/* NBG0をタイルモードへ設定 */
+	/* TODO */
+}
+
+/* NBG0へタイルで'A'を描画してみる */
+void put_tile_A(void)
+{
+	/* 色情報をVDP2 カラーRAMへ配置 */
+	/* TODO */
+
+	/* タイルデータをVDP2 RAMへ配置 */
+	/* TODO */
+
+	/*  */
+}
